f621fb019e0dd992a2e734e2c7b9597991df7484
Vadim <11475496+0c70pu5@users.noreply.github.com>
2020-07-01 10:44:40 +0300

    Delete source-code-pro.sh

--- source-code-pro.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-mkdir -p /tmp/adodefont
-cd /tmp/adodefont
-wget -q --show-progress -O source-code-pro.zip https://github.com/adobe-fonts/source-code-pro/archive/2.030R-ro/1.050R-it.zip
-unzip -q source-code-pro.zip -d source-code-pro
-mkdir -p /usr/local/share/fonts/
-sudo cp -v source-code-pro/*/OTF/*.otf /usr/local/share/fonts/
-fc-cache -f
-rm -rf source-code-pro{,.zip}
